5fd2b96b33c0b70c0e16025a89806e786c6c0aa6
Aastha <aasthamishra175@gmail.com>
2024-04-20 22:59:59 +0545

    Few more changes in styling and the content also take time to fade through js.

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,polit
 // getJoke();
 
 let getJoke = () => {
+    jokeContainer.classList.remove('fade');
     fetch(url)
         .then(data => data.json())
         .then(item => {
             jokeContainer.textContent = `${item.joke}`;
+            jokeContainer.classList.add('fade');
         });
 }
 btn.addEventListener('click', getJoke);
